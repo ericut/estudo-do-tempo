64e529bd1c927c986826be1fcb6b8c050de93f7e
Eric Li <ericut@gmail.com>
2022-08-18 14:09:04 -0300

    Melhoria do estudo do tempo

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # Estudo do Tempo
 
-Sim! Este projeto levou 9m14s para ser criado e publicado!
+Sim! Esta página levou 9m14s para ser criada e publicada!
 
 - `yarn create vite estudo-do-tempo --template react-ts`
+- `cd estudo-do-tempo`
 - `yarn`
 - `yarn dev`
-- Editar o CSS do iFrame para ficar 100% (já com o link compartilhado do YouTube)
+- `code .` (com o VS Code instalado)
+- Criar e editar CSS para o iFrame do YouTube (já com o link compartilhado do YouTube)
 - Escrever o texto comparativo do tempo (já com os links em mãos)
 - Entrar no GitHub e fazer o repositório
 - `git init`
@@ -17,6 +19,12 @@ Sim! Este projeto levou 9m14s para ser criado e publicado!
 - Selecionar o deploy baseado neste repositório acima
 - Aguardar o deploy!
 
-Há 5 anos eu demoraria 1 hora ou talvez 2 horas para fazer isso, mas a reflexão não é o tempo que eu levaria antes ou quão rápido eu sou hoje, é ter na prática e saber o caminho para solucionar pequenos problemas do dia a dia.
+## Reflexão
 
-Obs: Este Readme foi escrito após a criação deste mini-desafio. Ou seja faça a solução e escreva a documentação depois!
+Há 5 anos atrás eu demoraria 1 hora ou talvez 2 horas para fazer algo parecido, mas a reflexão não é o tempo que eu levaria antes ou quão rápido eu sou hoje, é que com a prática consegui com mínimo esforço criar e públicar uma pequena página.
+
+Obs: Este Readme foi escrito após a criação deste mini-desafio. Ou seja, faça a solução e escreva a documentação depois!
+
+## Melhorias
+
+- Após sua publicação me dediquei por 3m14s para fazer uma melhoria na listagem dos links.
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const lista = [
+    { title: "Ler a documentação básica do Vite", link: "https://vitejs.dev/guide/#scaffolding-your-first-vite-project" },
+    { title: "Fazer um Hello World em React", link: "https://www.youtube.com/watch?v=AKXpOUo10k0" },
+    { title: "Ver este vídeo do Firebase sobre Monorepos", link: "https://www.youtube.com/watch?v=9iU_IE6vnJ8" },
+    {
+      title: "Ler este artigo sobre testes com Jest (tem até um botão Listen para ouvir)",
+      link: "https://medium.com/swlh/how-to-testwith-jest-bfdb9bd40d86",
+    },
+    {
+      title: "Ou até mesmo ver esse React do Casimiro vendo vídeo do Corbucci em 1.2x",
+      link: "https://www.youtube.com/watch?v=z9NT4gAY3xk",
+    },
+  ];
 
   return (
     <div className="App">
@@ -16,29 +26,17 @@ function App() {
         <h1>Estudo do Tempo</h1>
         <p>Esta página levou 9m14s para ser criada e publicada!</p>
         <h2>Neste tempo você também conseguiria:</h2>
-        <p>
-          Ler a documentação básica do Vite
-          <br />
-          https://vitejs.dev/guide/#scaffolding-your-first-vite-project
-        </p>
-        <p>
-          Fazer um Hello World em React <br />
-          https://www.youtube.com/watch?v=AKXpOUo10k0
-        </p>
-        <p>
-          Ver este vídeo do Firebase sobre Monorepos
-          <br />
-          https://www.youtube.com/watch?v=9iU_IE6vnJ8
-        </p>
-        <p>
-          Ler este artigo sobre testes com Jest (tem até um botão Listen para ouvir)
-          <br />
-          https://medium.com/swlh/how-to-testwith-jest-bfdb9bd40d86
-        </p>
-        <p>
-          Ou até mesmo ver esse React do Casimiro vendo vídeo do Corbucci em 1.2x <br />
-          https://www.youtube.com/watch?v=z9NT4gAY3xk
-        </p>
+        {lista.map((item) => {
+          return (
+            <p>
+              {item.title}
+              <br />
+              <a href={item.link} target="_blank">
+                {item.link}
+              </a>
+            </p>
+          );
+        })}
         <h2>A decisão é sua!</h2>
       </div>
     </div>
